aacfbeef203f89c95f72aeea9ea0803433134f50
nune <145225213+gigirassy@users.noreply.github.com>
2024-12-19 23:49:02 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fetch = require('node-fetch');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
